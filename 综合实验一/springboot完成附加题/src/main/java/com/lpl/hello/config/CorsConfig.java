8ea8974801b2d4a4de0cda0d7b996349dcae0a55
李鹏龙 <lplgitee@user.noreply.gitee.com>
2021-11-19 11:46:38 +0800

    跨域配置

---
+++ 综合实验一/springboot完成附加题/src/main/java/com/lpl/hello/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.lpl.hello.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 配置跨域请求
+ */
+//@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders(CorsConfiguration.ALL)
+                .allowedMethods(CorsConfiguration.ALL)
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
